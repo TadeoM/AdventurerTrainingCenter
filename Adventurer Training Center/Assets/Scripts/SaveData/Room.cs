cca5fda653df10cab6e33e4cd4dd1aaa0bbb61e4
Tadeo <tmenichel@protonmail.com>
2021-02-14 13:57:40 -0500

    Saving and loading data properly!

--- Adventurer Training Center/Assets/Scripts/SaveData/Room.cs
+++ Adventurer Training Center/Assets/Scripts/SaveData/Room.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public string roomName;
-    public List<Room> additionalRooms;
     public List<string> unlockedHeroes;
     public List<string> unlockedMonsters;
     public List<string> unlockedWeapons;
     public List<string> unlockedArmors;
     public List<string> unlockedPassiveBonuses; // str+ , int+ , dex+ , damage+
 
-    public List<string> unlockedRooms;
+    public List<Room> unlockedRooms;
     public List<string> unlockedTrainers;
 
     public List<ScriptableObject> availableUpgrades;
     public List<ScriptableObject> unlockedUpgrades;
     // bonuses/armor/weapons have all the necessary stats, you add to all the entity's stats at the beginning of the dungeon.
     // go through the unlockedUpgrades and call an event that will pass the unlocked ability/entity/item (create 3 different methods in an event manager). Each method adds the passed in parameter to the 5 lists above, which will be stored in like, some account class
-
-    public void AddRoom(Room room)
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        additionalRooms.Add(room);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
--- Adventurer Training Center/Assets/Scripts/SaveData/SaveManager.cs
+++ Adventurer Training Center/Assets/Scripts/SaveData/SaveManager.cs
@@ -9,28 +9,56 @@ public class SaveManager : Singleton<SaveManager>
      * size
      * array of rooms
      */
-    public void SaveData(Room room)
+    public RoomSaveData trainingCenter;
+    public Room roomToSave;
+
+    private void Start()
     {
-        
+        //SaveData(roomToSave);
+        StartLoadRoom("TrainingCenter");
     }
 
-    public Room StartLoadRoom(string roomName)
+    public void SaveData(Room room)
     {
-        if (!HasFileOnDisk(roomName))
+        if(trainingCenter == null)
+        {
+            trainingCenter = new RoomSaveData();
+        }
+        trainingCenter.fileName = room.roomName;
+        trainingCenter.mainRooms = new List<string>();
+        trainingCenter.additionalRooms = new List<string>();
+
+        for (int a = 0; a < room.unlockedRooms.Count; a++)
+        {
+            trainingCenter.mainRooms.Add(room.unlockedRooms[a].name);
+            
+            for (int b = 0; b < room.unlockedRooms[a].unlockedRooms.Count; b++)
+            {
+                trainingCenter.additionalRooms.Add($"{room.unlockedRooms[a].unlockedRooms[b].name}:{room.unlockedRooms[a].name}");
+            }
+        }
+
+        // same thing for trainers once we have that data in-game
+        string json = JsonUtility.ToJson(trainingCenter);
+        File.WriteAllText(GetPath(trainingCenter.fileName), json);
+    }
+
+    public RoomSaveData StartLoadRoom(string roomName)
+    {
+        /*if (!HasFileOnDisk(roomName))
         {
             return null;
-        }
+        }*/
+        trainingCenter = new RoomSaveData();
+        string json = File.ReadAllText(GetPath(roomName));
+        JsonUtility.FromJsonOverwrite(json, trainingCenter);
+
+        Debug.Log(trainingCenter.additionalRooms);
         try
         {
-            string json = File.ReadAllText(GetPath(roomName));
-            JsonUtility.FromJsonOverwrite(json, this);
-            // get the JSON "room0: {additionalRooms: [ room1: {additionalRooms:[room3], room2]"
-            /*Room room;
-            for (int i = 0; i < room.additionalRooms.Count; i++)
-            {
-                IteratedLoadRoom(room.additionalRooms[i].name);
-            }*/
-            return null;
+            /*string json = File.ReadAllText(GetPath("TrainingCenter"));
+            JsonUtility.FromJsonOverwrite(json, trainingCenter);*/
+            return trainingCenter;
         }
         catch
         {
