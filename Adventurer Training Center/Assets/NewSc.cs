4e8e0a1e21715dd633522a60f6914d2f5a420d23
Tadeo <tmenichel@protonmail.com>
2021-05-06 16:23:53 -0400

    Make the dungeon prep great again

---
+++ Adventurer Training Center/Assets/NewSc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSc : MonoBehaviour
+{
+    public int runSpeed;
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
